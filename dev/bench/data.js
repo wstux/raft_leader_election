b8b4c666773e1d0e48ded6941687776ab130b003
Chistyakov Alexander <wstux1@gmail.com>
2026-08-27 11:43:00 +0300

    + ci: Grouping charts on a single plot by benchmarks. History restored;

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,6 +16,329 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
+          "id": "539624e1b6ce430129567471b3b46a984f5c318d",
+          "message": "* all: #26 Merge remote-tracking branch 'wstux/bugfix/is_stop';",
+          "timestamp": "2026-08-25T19:57:37+03:00",
+          "tree_id": "1e23aff355b8c8a2b513ed8bd141f143dc84808a",
+          "url": "https://github.com/wstux/raft_leader_election/commit/539624e1b6ce430129567471b3b46a984f5c318d"
+        },
+        "date": 1787677211327,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Leader Election (Real Time)::3 nodes",
+            "value": 41.36764421000009,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 0.33630375000000035 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election (CPU Time)::3 nodes",
+            "value": 0.33630375000000035,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 0.33630375000000035 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election (Real Time)::5 nodes",
+            "value": 42.08667316999936,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 0.763597520000004 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election (CPU Time)::5 nodes",
+            "value": 0.763597520000004,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 0.763597520000004 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election (Real Time)::7 nodes",
+            "value": 42.52440105000346,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 1.2858168999999986 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election (CPU Time)::7 nodes",
+            "value": 1.2858168999999986,
+            "unit": "ms",
+            "extra": "iterations: 100\ncpu: 1.2858168999999986 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::3 nodes",
+            "value": 632.7276119000061,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 0.7850358000000224 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::3 nodes",
+            "value": 0.7850358000000224,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 0.7850358000000224 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::5 nodes",
+            "value": 589.1793224999986,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 1.3733478000000021 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::5 nodes",
+            "value": 1.3733478000000021,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 1.3733478000000021 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::7 nodes",
+            "value": 571.4614855999997,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 1.7098741999999945 ms\nthreads: 1"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::7 nodes",
+            "value": 1.7098741999999945,
+            "unit": "ms",
+            "extra": "iterations: 10\ncpu: 1.7098741999999945 ms\nthreads: 1"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::1 threads",
+            "value": 4.459247273738747,
+            "unit": "ns",
+            "extra": "iterations: 157874444\ncpu: 4.45881164908489 ns\nthreads: 1"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::2 threads",
+            "value": 28.1611041824231,
+            "unit": "ns",
+            "extra": "iterations: 24635096\ncpu: 28.159601326497768 ns\nthreads: 2"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::4 threads",
+            "value": 92.68074425438567,
+            "unit": "ns",
+            "extra": "iterations: 7544356\ncpu: 92.56267002776647 ns\nthreads: 4"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::8 threads",
+            "value": 149.8874538720559,
+            "unit": "ns",
+            "extra": "iterations: 10423184\ncpu: 89.08632381429705 ns\nthreads: 8"
+          },
+          {
+            "name": "Request to List (Real Time)::1 threads",
+            "value": 17.74469018846682,
+            "unit": "ns",
+            "extra": "iterations: 39502532\ncpu: 17.74331680814789 ns\nthreads: 1"
+          },
+          {
+            "name": "Request to List (Real Time)::2 threads",
+            "value": 18.16592570066908,
+            "unit": "ns",
+            "extra": "iterations: 38494344\ncpu: 18.165731802053834 ns\nthreads: 2"
+          },
+          {
+            "name": "Request to List (Real Time)::4 threads",
+            "value": 34.77990504077897,
+            "unit": "ns",
+            "extra": "iterations: 20185928\ncpu: 34.73710334248691 ns\nthreads: 4"
+          },
+          {
+            "name": "Request to List (Real Time)::8 threads",
+            "value": 58.242427653647674,
+            "unit": "ns",
+            "extra": "iterations: 17322504\ncpu: 34.69156497236198 ns\nthreads: 8"
+          },
+          {
+            "name": "Request Lock List (Real Time)::1 threads",
+            "value": 1.3646141277387434,
+            "unit": "ns",
+            "extra": "iterations: 513066680\ncpu: 1.364529472075638 ns\nthreads: 1"
+          },
+          {
+            "name": "Request Lock List (Real Time)::2 threads",
+            "value": 1.365822639091638,
+            "unit": "ns",
+            "extra": "iterations: 512363118\ncpu: 1.3657304661808223 ns\nthreads: 2"
+          },
+          {
+            "name": "Request Lock List (Real Time)::4 threads",
+            "value": 2.7414586284864244,
+            "unit": "ns",
+            "extra": "iterations: 253100336\ncpu: 2.7375833748399305 ns\nthreads: 4"
+          },
+          {
+            "name": "Request Lock List (Real Time)::8 threads",
+            "value": 4.718932173428343,
+            "unit": "ns",
+            "extra": "iterations: 262538288\ncpu: 2.7311668993590787 ns\nthreads: 8"
+          },
+          {
+            "name": "Serialize Message (Real Time)::Base",
+            "value": 0.5460552518787369,
+            "unit": "ns",
+            "extra": "iterations: 1283001549\ncpu: 0.5459759620290217 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wstux1@gmail.com",
+            "name": "wstux",
+            "username": "wstux"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "462ea3ab3a419c661c5ae65e20671340f840cb14",
+          "message": "* all: #27 Merge remote-tracking branch 'wstux/feature/github_actions';",
+          "timestamp": "2026-08-25T22:48:58+03:00",
+          "tree_id": "9966b62c58f71920b884148f5c690874d0abf7a0",
+          "url": "https://github.com/wstux/raft_leader_election/commit/462ea3ab3a419c661c5ae65e20671340f840cb14"
+        },
+        "date": 1787687517400,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Leader Election (Real Time)::3 nodes",
+            "value": 41.44654836000228,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election (CPU Time)::3 nodes",
+            "value": 0.37540378000000096,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election (Real Time)::5 nodes",
+            "value": 42.011793209998984,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election (CPU Time)::5 nodes",
+            "value": 0.8200491100000038,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election (Real Time)::7 nodes",
+            "value": 43.94043772000174,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election (CPU Time)::7 nodes",
+            "value": 1.4519588100000018,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::3 nodes",
+            "value": 695.2629804000082,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::3 nodes",
+            "value": 0.9825335999999574,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::5 nodes",
+            "value": 609.2897692999856,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::5 nodes",
+            "value": 1.3174234000000062,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (Real Time)::7 nodes",
+            "value": 565.8748855000027,
+            "unit": "ms"
+          },
+          {
+            "name": "Leader Election Default (CPU Time)::7 nodes",
+            "value": 1.9224650999999817,
+            "unit": "ms"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::1 threads",
+            "value": 5.713500969813915,
+            "unit": "ns"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::2 threads",
+            "value": 57.25875226000947,
+            "unit": "ns"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::4 threads",
+            "value": 115.27677727747839,
+            "unit": "ns"
+          },
+          {
+            "name": "Check Contact Quorum (Real Time)::8 threads",
+            "value": 188.23478909976458,
+            "unit": "ns"
+          },
+          {
+            "name": "Request to List (Real Time)::1 threads",
+            "value": 22.852178713471936,
+            "unit": "ns"
+          },
+          {
+            "name": "Request to List (Real Time)::2 threads",
+            "value": 23.418577088253617,
+            "unit": "ns"
+          },
+          {
+            "name": "Request to List (Real Time)::4 threads",
+            "value": 45.20300812262107,
+            "unit": "ns"
+          },
+          {
+            "name": "Request to List (Real Time)::8 threads",
+            "value": 78.06587567468266,
+            "unit": "ns"
+          },
+          {
+            "name": "Request Lock List (Real Time)::1 threads",
+            "value": 1.7829470828268246,
+            "unit": "ns"
+          },
+          {
+            "name": "Request Lock List (Real Time)::2 threads",
+            "value": 1.762516207094626,
+            "unit": "ns"
+          },
+          {
+            "name": "Request Lock List (Real Time)::4 threads",
+            "value": 3.520957245655817,
+            "unit": "ns"
+          },
+          {
+            "name": "Request Lock List (Real Time)::8 threads",
+            "value": 6.748353066215472,
+            "unit": "ns"
+          },
+          {
+            "name": "Serialize Message (Real Time)::Base",
+            "value": 0.7033844075851101,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wstux1@gmail.com",
+            "name": "wstux",
+            "username": "wstux"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
           "id": "041311e81cadf8247af28407e8affcbf21be0241",
           "message": "* all: #29 Merge remote-tracking branch 'wstux/feature/update_cmake';",
           "timestamp": "2026-08-26T21:12:16+03:00",
